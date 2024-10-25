1e640a649729e0539ef5967916db5521da99bc08
Sarah <saraharguetaluv@gmail.com>
2024-10-24 20:44:04 -0400

    mongodb integration

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,67 +1,90 @@
 const model = require('../models/item');
-const { v4: uuidv4} = require('uuid');
 const { upload } = require('../middleware/fileUpload');
 
 // show items on items page
-exports.display = (req, res) => {
-    let items = model.find(); 
-    items.sort((a, b) => a.price - b.price); 
-    if (req.query.search !== undefined) {
+exports.display = (req, res, next) => {
+
+    let query = {}
+    if(req.query.search !== undefined && req.query.search.trim() !== ""){
         const search = req.query.search.toLowerCase();
-        items = items.filter(item => {
-            const itemName = item.name ? item.name.toLowerCase() : "";
-            const itemDetails = item.details ? item.details.toLowerCase() : "";
-            return itemName.includes(search) || itemDetails.includes(search);
-        });
+
+        query = {
+            $or: [
+                {title: { $regex: search, $options: 'i' } },
+                {details: { $regex: search, $options: 'i' } } 
+            ]
+        }
     }
-    res.render('./item/items', { items });
-    console.log('using display controller');
+
+    model.find(query).sort({price: 1})
+    .then(items => res.render('./item/items', {items}))
+    .catch(err => next(err))
 };
 
 // show page for making a new item
 exports.new = (req, res) => {
     res.render('./item/new');
-    console.log('using new export controller');
 }
 
 // show page for item details
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    let item = model.findById(id);
-    if(item){
-        res.render('./item/item', {item});
-        console.log('using show export controller');
-    } else {
-        let err = new Error('Cannot find an item with ID ' + id);
-        err.status = 404;
-        next(err);
-    }
+    if(!id.match(/^[0-9a-fA-F]{24}$/)){
+        let err = new Error('Invalid item id');
+        err.status = 400;
+        return next(err);
+    } 
+    model.findById(id)
+    .then(item => {
+        if(item){
+            res.render('./item/item', {item});
+        }else {
+            let err = new Error('Cannot find an item with ID ' + id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    
 }
 
-exports.create = (req, res) => {
+exports.create = (req, res, next) => {
     upload(req, res, (err) =>{
         if(err){
             res.status(400);
         }
-        let item = req.body;
-        item.id = uuidv4();
+        let item = new model(req.body);
         item.image = '/images/' + req.file.filename;
-        model.save(item);
-        res.redirect('/items')
-    }) 
+        item.save()
+        .then(() =>{
+            res.redirect('/items')
+        })
+        .catch(err => {
+            if(err.name === 'ValidationError'){
+                err.status = 400;
+            }
+            next(err);
+        });
+    });
 }
 
 exports.edit = (req, res, next) => {
     let id = req.params.id;
-    let item = model.findById(id);
-    console.log("edit controller used");
-    if(item){
+
+    if(!id.match(/^[0-9a-fA-F]{24}$/)){
+        let err = new Error('Invalid item id');
+        err.status = 400;
+        return next(err);
+    } 
+    model.findById(id)
+    .then(item => {
+        if(item){
         res.render('./item/edit', {item});
-    } else {
-        let err = new Error('Cannot find an item with ID ' + id);
-        err.status = 404;
-        next(err);
-    }
+        }else {
+            let err = new Error('Cannot find an item with ID ' + id);
+            err.status = 404;
+            next(err);
+        }
+    });  
 }
 
 exports.update = (req, res, next) => {
@@ -71,29 +94,49 @@ exports.update = (req, res, next) => {
         }
         let item = req.body;
         let id = req.params.id;
-        let editItem = model.findById(id);
-        console.log("image: ", editItem.image);
-            if(req.file){
-                item.image = '/images/' + req.file.filename;
-            }
-            if(model.updateById(id, item)){
-                console.log("Image: ",item.image);
+
+        if(!id.match(/^[0-9a-fA-F]{24}$/)){
+            let err = new Error('Invalid item id');
+            err.status = 400;
+            return next(err);
+        }
+
+        model.findByIdAndUpdate(id, item, {useFindAndModify: false, runValidators: true})
+        .then(item => {
+            if(item){
                 res.redirect('/items/' + id);
             }else{
                 let err = new Error('Cannot find an item with ID ' + id);
                 err.status = 404;
                 next(err);
             }
+        })
+        .catch(err => {
+            if(err.name === 'ValidationError'){
+                err.status = 400;
+            }
+            next(err)
+        })
     });
 }
 
 exports.delete = (req, res, next) => {
     let id = req.params.id;
-    if(model.deleteById(id)){
-        res.redirect('/items');
-    }else{
-        let err = new Error('Cannot find an item with ID' + id);
-        err.status = 404;
-        next(err);
+    if(!id.match(/^[0-9a-fA-F]{24}$/)){
+        let err = new Error('Invalid item id');
+        err.status = 400;
+        return next(err);
     }
+
+    model.findByIdAndDelete(id, {useFindAndModify: false})
+    .then(item => {
+        if(item){
+            res.redirect('/items');
+        }else{
+            let err = new Error('Cannot find an item with ID' + id);
+            err.status = 404;
+            next(err);
+        }
+    }) 
+    .catch(err => next(err));
 }
